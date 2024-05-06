efa336e7674c6683f12d229d893763e23079803e
SaqibTahir0201 <sqbmaaz@gmail.com>
2024-05-07 01:47:14 +0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,123 @@
-const date = document.getElementById("date");
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
+// // Today is Monday, May 6, 2024
 
-//?  FUNCTION FOR TIMER
+// // Function to get the current date
+// function getCurrentDate() {
+//   let currentDate = new Date();
+//   return currentDate;
+// }
 
-let time = setInterval(function () {
-  let now = new Date();
-  console.log(now.getDate);
-  date.innerText = now.getDate();
-  hours.innerText = now.getHours();
-  minutes.innerText = now.getMinutes();
-  seconds.innerText = now.getSeconds();
-}, 1000);
+// // Function to get the day of the week
+// function getDayOfWeek(date) {
+//   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//   return days[date.getDay()];
+// }
+
+// // Function to get the month
+// function getMonth(date) {
+//   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//   return months[date.getMonth()];
+// }
+
+// // Function to get the day of the month
+// function getDayOfMonth(date) {
+//   return date.getDate();
+// }
+
+// // Function to get the year
+// function getYear(date) {
+//   return date.getFullYear();
+// }
+
+// // FF9900
+// // Get the current date and time
+// let currentDate = getCurrentDate();
+// let dayOfWeek = getDayOfWeek(currentDate);
+// let month = getMonth(currentDate);
+// let dayOfMonth = getDayOfMonth(currentDate);
+// let year = getYear(currentDate);
+
+
+// if (condition ===) {
+//   else if else if (){}
+// }
+
+// // Display the current date and time
+// console.log(`Today is ${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`);
+
+// // Example of using an external JavaScript file
+// function navigation() {
+//   console.log("Navigation menu is loaded");
+// }
+
+// // Example of using multiple external JavaScript files
+// function slideshow() {
+//   console.log("Slideshow is loaded");
+// }
+
+// // Example of adding custom JavaScript code to a page
+// alert('Hello world!');
+
+// // Example of using JavaScript to write to a web page
+// document.write("This text was written by JavaScript");
+
+// // Example of using JavaScript to get user input
+// let userName = prompt("What is your name?");
+// console.log(`Hello, ${userName}`);
+
+// // Example of using JavaScript to make decisions
+// let age = prompt("How old are you?");
+// if (age === 18) {
+//   console.log('');
+// } else {
+//   console.log("You are a minor");
+// }
+// 7898797879787897987987789798
+// // Example of using JavaScript to loop through an array
+// let colors = ["red", "green", "blue", "yellow", "orange"];
+// for (let i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
+// debugger
+// // Example of using JavaScript to create a function that takes arguments
+// function greet(name, age) {
+//   console.log(`Hello, ${name}! You are ${age} years old.`);
+
+
+// return true 
+
+// }
+
+// let age = () => {
+
+// }
+
+// isEven = true
+// switch (key) {
+//   case value:
+    
+//     break;
+// new Date()
+//   default:
+//     break;
+// }
+// greet("John", 30);
+
+// // Example of using JavaScript to create an object
+// let person = {
+//   name: "Jane",
+//   age: 25,
+//   occupation: "Software Engineer"
+// };
+// console.log(`Name: ${person.name}, Age: ${person.age}, Occupation: ${person.occupation}`);
+
+// // Example of using JavaScript to create an array of objects
+// let people = [
+//   { name: "John", age: 30, occupation: "Doctor" },
+//   { name: "Jane", age: 25, occupation: "Software Engineer" },
+//   { name: "Bob", age: 40, occupation: "Lawyer" }
+// ];
+// for (let i = 0; i < people.length; i++) {
+//   console.log(`Name: ${people[i].name}, Age: ${people[i].age}, Occupation: ${people[i].occupation}`);
+// }
+
+
